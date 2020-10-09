d0920a35d39091243dfcbe38b505070992d88bbc
Jeniffer Teixeira de Oliveira <oliveira.jeniffer@live.com>
2020-10-09 16:05:16 -0300

    Created a function to make the API call and set up the App.js to trigger it as soon as the component is mounted. Then, the App.js also saves the data into the state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
 import{Cards, Chart, CountryPicker} from './components'
-
 import styles from './App.module.css'
+import {fetchData} from './api'
 
 class App extends React.Component {
+
+  state={
+    data:{},
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+
   state = {  }
   render() { 
     return ( 
