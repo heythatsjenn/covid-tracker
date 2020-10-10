c65b5d8c14b71f8c1cc117dd194cec822184c187
Jeniffer Teixeira de Oliveira <oliveira.jeniffer@live.com>
2020-10-10 01:38:06 -0300

    Uptaded the api to also fetch names of countries. Used that data to fill the country picker that I made with Material UI.

--- src/api/index.js
+++ src/api/index.js
@@ -17,6 +17,16 @@ export const fetchDailyData = async () =>{
         const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
         return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
     } catch (error) {
-        
+        console.log(error);
     }
 }
+
+export const fetchCountries = async () => {
+    try {
+      const { data: { countries } } = await axios.get(`${url}/countries`);
+  
+      return countries.map((country) => country.name);
+    } catch (error) {
+      return error;
+    }
+};
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { fetchCountries } from "../../api";
+import {FormControl, NativeSelect} from '@material-ui/core';
+import styles from './CountryPicker.module.css'
 
 const CountryPicker = () => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+          setFetchedCountries(await fetchCountries());
+        };
+        fetchAPI();
+      });
+
     return ( 
-    <div>
-        <h1>CountryPicker</h1>
-    </div> );
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="daily-us">Daily US</option>
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
+        );
 }
  
 export default CountryPicker;
