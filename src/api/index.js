ef043f2cbe00e3b24c375e6e0a166664593c0a97
Jeniffer Teixeira de Oliveira <oliveira.jeniffer@live.com>
2020-10-10 00:20:10 -0300

    Fetched the daily data from the US and set up a graph using this information.

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,12 @@ export const fetchData = async () =>{
         
     }
 }
+
+export const fetchDailyData = async () =>{
+    try {
+        const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+        return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    } catch (error) {
+        
+    }
+}
